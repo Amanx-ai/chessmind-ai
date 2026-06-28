83eb0cc6a04050046be4173fc69f8938ef3d9bbd
Amn <223648672+Amanx-ai@users.noreply.github.com>
2026-06-28 19:09:46 +0530

    Enable legal chess moves with chess.js

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 
 function ChessBoard() {
+  const [game, setGame] = useState(new Chess());
+
+  const chessboardOptions = {
+    id: "BasicBoard",
+    position: game.fen(),
+    onPieceDrop: ({
+      sourceSquare,
+      targetSquare,
+    }: {
+      sourceSquare: string;
+      targetSquare: string | null;
+    }) => {
+      if (!targetSquare) return false; // dropped off the board
+
+      const gameCopy = new Chess(game.fen());
+
+      try {
+        gameCopy.move({
+          from: sourceSquare,
+          to: targetSquare,
+          promotion: "q",
+        });
+        setGame(gameCopy);
+        return true;
+      } catch {
+        return false; // illegal move
+      }
+    },
+  };
+
   return (
     <div style={{ width: "500px" }}>
-      <Chessboard />
+      <Chessboard options={chessboardOptions} />
     </div>
   );
 }
