6cdfcda484a0e5703b367fc1c768fc6694fb2d36
Amn <223648672+Amanx-ai@users.noreply.github.com>
2026-06-30 13:12:31 +0530

    feat(api): accept FEN in best move endpoint

--- backend/app/api/analysis.py
+++ backend/app/api/analysis.py
@@ -5,20 +5,21 @@ router = APIRouter()
 
 stockfish = StockfishService()
 
+
 @router.get("/ping")
 def ping():
     return {
         "message": "analysis router working"
     }
 
+
 @router.get("/service-status")
 def service_status():
     return stockfish.ping()
 
+
 @router.get("/best-move")
-def best_move():
+def best_move(fen: str):
     return {
-        "best_move": stockfish.get_best_move(
-            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-        )
-    }    
+        "best_move": stockfish.get_best_move(fen)
+    }
